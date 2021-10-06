42fe99a8062f23bd99f1e0a0c80e1db9cd9c10c6
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2021-10-06 19:18:32 +0200

    Add files via upload

---
+++ vac_robot.ino
@@ -0,0 +1,20 @@
+
+const int MOTOR_LEFT1 = 3; //digital 3 motor left
+const int MOTOR_LEFT2 = 5; // digital 5 motor left
+const int MOTOR_RIGHT3 = 6; //digital 6 motor right
+const int MOTOR_RIGHT4 = 9;//digital 9 motor right
+
+const int BUMBER1 = 10; //digital 10 bumber 1
+const int BUMBER2 = 11; //digital 11 bumber 2
+
+
+
+void setup() {
+  // put your setup code here, to run once:
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
