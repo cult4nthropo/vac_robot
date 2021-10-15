f2ada2252ce72d5fce23b5ffa17364bd2e9af283
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2021-10-15 21:18:10 +0200

    Add files via upload

--- vac_robot.ino
+++ vac_robot.ino
@@ -1,11 +1,24 @@
+//digital pins motor
 
-const int MOTOR_LEFT1 = 3; //digital 3 motor left
-const int MOTOR_LEFT2 = 5; // digital 5 motor left
-const int MOTOR_RIGHT3 = 6; //digital 6 motor right
-const int MOTOR_RIGHT4 = 9;//digital 9 motor right
+const int MOTOR_RIGHT1 = 3; //digital 3 motor left
+const int MOTOR_RIGHT2 = 5; // digital 5 motor left
+const int MOTOR_LEFT1 = 6; //digital 6 motor right
+const int MOTOR_LEFT2 = 9;//digital 9 motor right
 
+//digital pins bumber
 const int BUMBER1 = 10; //digital 10 bumber 1
 const int BUMBER2 = 11; //digital 11 bumber 2
+
+//fan output
+const int FAN = 12; //MosFet to fan
+
+const int LED = 13; 
+
+//analog sensors
+const int SENSOR_LEFT = 0; //analog 0
+const int SENSOR_RIGHT = 1; //analog 1
+
+const int BATTERY = 4; analog 3
 
 
 
