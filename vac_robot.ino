ae52d34007e7ddf9d553b8e16ccac662f8485331
Kathleen <88250122+cult4nthropo@users.noreply.github.com>
2021-11-14 18:26:38 +0100

    updatet setup with all basic functions

--- vac_robot.ino
+++ vac_robot.ino
@@ -1,108 +1,108 @@
-//digital pins motor
 
-const int MOTOR_RIGHT1 = 3; //digital 3 motor left
-const int MOTOR_RIGHT2 = 5; // digital 5 motor left
-const int MOTOR_LEFT1 = 6; //digital 6 motor right
-const int MOTOR_LEFT2 = 9;//digital 9 motor right
 
-//digital pins bumber
-const int BUMBER1 = 10; //digital 10 bumber 1
-const int BUMBER2 = 11; //digital 11 bumber 2
+//analog pins for sensors
+const int SENSOR_LEFT1 = 0; //analogIn 0 for left sensor
+const int SENSOR_RIGHT1 = 1; //analogIn 1 for right sensor
+const int SENSOR_LEFT2 = 2; //analogIn 2 for left sensor
+const int SENSOR_RIGHT2 = 3; //analogIn 3 for right sensor
 
-//fan output
-const int FAN = 12; 
+const int BATTERY = 4; //analogIn 4 for reading the battery
 
-const int LED = 13; 
+const int LED = 13; //digital pin 13 for LED while testing
 
-//analog sensors
-const int SENSOR_LEFT = 0; //analog 0 //check where the other both sensor contacts come from
-const int SENSOR_RIGHT = 1; //analog 1
+const int FAN_MOTOR = 12; //digital pin 12 for fan motor over h bridge
 
-const int BATTERY = 4; 
+//digital pins for motors
+const int MOTOR_LEFT1 = 9;
+const int MOTOR_LEFT2 = 6;
 
-const float BATTERY_CHARGED = 11.1;
-const float PWM_MAX = 130; //check this again (should be correct vor 11.1V)
-const float PWM_MIN = 75;// check this again (should be vor 9V or what?
-const int SENSOR_MIN = 30;
+const int MOTOR_RIGHT1 = 5;
+const int MOTOR_RIGHT2 = 3;
+
+const int BUMBER1 = 11; //digital pin 11 for the bumber2
+const int BUMBER2 = 10; //digital pin 10 for bumber2
+
+//constants to define control values
+const float BATTERY_CHARGED = 12.68; //check this in battery manual; voltage when battery fully charged
+
+//max power for motors - depends on battery, check this
+const int MAX_POWER = 160; //depents on battery output 12V POWER_MAX = 170, 10V output MAX_POWER = 140
+const int MIN_POWER = 70; //for 9V MIN_POWER = 128
+//minimum distance for sensors
+const int MIN_DISTANCE = 30;
 
 //variables
-int bumberState = 0;
-int counter = 0;
-boolean control = true;
+int bumberState = 0; //to read the push button status
+boolean batteryControl = true;
+int counter = 0; //avoids infinite loop
 
-
+//begin of code, first setting the pinModes for in and output
 void setup() {
-  // put your setup code here, to run once:
-  //initialize outputs and inputs
-  pinMode(MOTOR_RIGHT1, OUTPUT);
-  pinMode(MOTOR_RIGHT2, OUTPUT);
-  pinMode(MOTOR_LEFT1, OUTPUT);
-  pinMode(MOTOR_LEFT2, OUTPUT);
-  pinMode(FAN, OUTPUT);
+pinMode(FAN_MOTOR, OUTPUT);
+pinMode(MOTOR_LEFT1, OUTPUT);
+pinMode(MOTOR_LEFT2, OUTPUT);
+pinMode(MOTOR_RIGHT1, OUTPUT);
+pinMode(MOTOR_RIGHT2, OUTPUT);
 
-  pinMode(SENSOR_LEFT, INPUT); //see sensor const: where it comes from determines, which pinMode it should have
-  pinMode(SENSOR_RIGHT, INPUT);
+pinMode(SENSOR_LEFT1, INPUT);
+pinMode(SENSOR_LEFT2, INPUT);
+pinMode(SENSOR_RIGHT1, INPUT);
+pinMode(SENSOR_RIGHT2, INPUT);
 
-  pinMode(BUMBER1, INPUT_PULLUP); //corrected. I thought just INPUT - try to understand PULLUP
-  pinMode(BUMBER2, INPUT_PULLUP); //try to understand PULLUP
+pinMode(BUMBER1, INPUT_PULLUP);
+pinMode(BUMBER2, INPUT_PULLUP);
 
-  pinMode(LED, OUTPUT);
+pinMode(BATTERY, INPUT);
 
-  pinMode(BATTERY, INPUT);
-  
-  blinkLED(5,1);
-  
-  // wait 5s and initialize fan, if voltage ok
-  if(readBattery(BATTERY)>11.1){
-    digitalWrite(FAN, HIGH);
-    delay(1000);
-    else:
-    //nothing
-  }
-  
+//Initialize and setting data rate
+Serial.begin(9600);
+
+// small if else: if battery is full just turn fan on to avoid overloading of battery (I guess) 
 }
 
 //functions
-
-void blinkLED(){
+//read the distance and return it in cm
+//adressing of sensors in the loop
+double readSensor (int sensor){
+  double distance = pow(analogRead(sensor), -0.857); //messured distance is the base, -0,857 the exponent
+  return (distance * 167.9);
   
-}
+  }
 
-void sensorDistance(){
- 
-}
+//functions for motors: one to go forward, one to got right, one to go left, one to go backwards, one to 
+void moveForward(int moveTime){
+  
+  }
 
-void bothMotors(){
- 
-}
+//stop all the motors
+void stopMotors() {
+  analogWrite(MOTOR_LEFT1, 0);
+  analogWrite(MOTOR_LEFT2, 0);
+  analogWrite(MOTOR_RIGHT1, 0);
+  analogWrite(MOTOR_RIGHT2, 0);
+  }
+  
+//function to read the battery state and returns it
+float readBattery(int BATTERY){
+  int readBATTERY;
+  float voltage;
+  readBATTERY = analogRead(BATTERY);
+  voltage = (((readBATTERY * 4.9) / 1000) * BATTERY_CHARGED) / 5; //honestly don't understand why, cause I don't know in which currency the arduino delivers its output
+  return voltage;
+  }
 
-void leftMotor(){
-
-}
-
-void rightMotor(){
-
-}
-
-void backwardMotor(){
-
-}
-
-void stopMotor(){
-  digitalWrite(MOTOR_LEFT1,0);
-  digitalWrite(MOTOR_LEFT2,0);
-  digitalWrite(MOTOR_RIGHT1,0);
-  digitalWrite(MOTOR_RIGHT2,0);
-}
-
-double readBattery(){
- 
-}
-
-void batteryControl(){
-
-}
-
+//function to turn everything of, if battery state is at min
+void batteryCritical(int BATTERY) {
+  float batteryStatus;
+  batteryStatus = readBattery(BATTERY);
+  if(batteryStatus <= 11.1){
+    batteryControl = false;
+    }
+   else{
+    //shouldn't there be anything?? something like continue?
+    }
+  }
+  
 void loop() {
   // put your main code here, to run repeatedly:
   //set everything up, if battery is at max
